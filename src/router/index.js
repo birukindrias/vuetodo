7e7bd86be04bd6a15dd486bb95380730fcf43809
birukindrias <birukindrias@gmail.com>
2023-02-24 02:23:14 -0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+    ,
+    {
+      path: '/Main',
+      name: 'Main',
+      // route level code-splitting
+      // this generates a separate chunk (Main.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MainView.vue')
+    }
+  ]
+
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,55 @@
+<script >
+export default {
+  data() {
+    return {
+      msg: 'mainhome',
+      val: 2,
+      array: ['name'],
+      txt: 'main',
+      indexofval: 0
+    }
+  },
+  methods: {
+    valadd() {
+      return this.val++;
+    },
+    add() {
+      return this.array.unshift(this.txt)
+    }, selected(el) {
+      console.log(this.array[el]);
+      this.indexofval = this.array[el]
+      // console.log(this.indexofval);
+    }
+    ,
+    update() {
+      this.array[this.indexofval] = this.txt
+
+      console.log(this.array[this.indexofval]);
+
+    }
+
+    ,
+    dele() {
+      this.array.pop([this.indexofval])
+
+    }
+  },
+}
+</script>
+
+<template>
+  <main>
+    {{ val }}
+    <div v-for="(ar, index) in array" :key="index">
+      <div @click="selected(index)">
+        {{ ar }}
+
+      </div>
+    </div>
+    <input v-model="txt">
+
+    <button @click="add">ad</button>
+    <button @click="update">update</button>
+    <button @click="dele">dele</button>
+  </main>
+</template>
